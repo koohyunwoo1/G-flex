b997088fb2a5f82b7c6caf2d7e874fd2d48b06e3
구현우 <kjklovekhw@gmail.com>
2024-05-12 17:30:12 +0900

    README 작성 / accounts, movies, articles app create

--- mypjt/urls.py
+++ mypjt/urls.py
@@ -19,5 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myapp/', include('myapp.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('articles/', include('articles.urls')),
+    path('movies', include('movies.urls')),
 ]
