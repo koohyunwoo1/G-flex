ea43cdec69846fffb9b63e49cc247eefd03c806e
구현우 <kjklovekhw@gmail.com>
2024-05-15 18:22:42 +0900

    Data add

--- back/accounts/urls.py
+++ back/accounts/urls.py
@@ -2,6 +2,3 @@ from django.urls import path
 from . import views
 
 app_name = 'accounts'
-urlpatterns = [
-    path('', views.index, name='index')
-]
--- back/movies/urls.py
+++ back/movies/urls.py
@@ -2,6 +2,3 @@ from django.urls import path
 from . import views
 
 app_name = 'movies'
-urlpatterns = [
-    path('', views.index, name='index')
-]
