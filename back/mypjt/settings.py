12593db91f9ff0f8af65b687faff10b116b86374
구현우 <kjklovekhw@gmail.com>
2024-05-17 08:55:45 +0900

    url 수정

--- back/mypjt/settings.py
+++ back/mypjt/settings.py
@@ -150,8 +150,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8080',
-    'http://127.0.0.1:8001',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
 ]
 
 
