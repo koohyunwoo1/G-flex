e7294c170f5a58bc3b0088ecce43e39eb1709ba1
김구태 <gootae0220@naver.com>
2024-05-16 10:45:50 +0900

    code restart, conflict fixed

--- back/movies/views.py
+++
@@ -1,8 +0,0 @@
-from .models import Actor, Genre, Movie, MoodTag
-from accounts.models import User
-
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-
