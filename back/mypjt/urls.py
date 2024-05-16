063b770bad85212c72360e6139411c48024424cf
김구태 <gootae0220@naver.com>
2024-05-16 12:47:25 +0900

    something conflict

--- back/mypjt/urls.py
+++ back/mypjt/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('api/v1/articles/', include('articles.urls')),
-    path('api/v1/movies/', include('movies.urls')),
+    path('articles/', include('articles.urls')),
+    path('movies/', include('movies.urls')),
 ]
