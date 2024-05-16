a5834ffa0ba1e6bd3605ece359e2bc30eda07cd7
김구태 <gootae0220@naver.com>
2024-05-16 10:43:05 +0900

    code  restart

--- back/movies/admin.py
+++ back/movies/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Movie
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'overview', 'words', 'like_users', 'release_date', 'genres', 'poster_url', )
+
+
+
+admin.site.register(Movie)
---
+++ back/movies/views.py
@@ -0,0 +1,8 @@
+from .models import Actor, Genre, Movie, MoodTag
+from accounts.models import User
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
