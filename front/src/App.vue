17e0331eaacf7115c82564a9277f600136adfbc7
구현우 <kjklovekhw@gmail.com>
2024-05-17 10:02:16 +0900

    Login/LogOut 기능 추가 구현

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,46 +1,48 @@
 <template>
   <div>
     <div class="navbar">
-      <RouterLink :to="{ name: 'HomeView' }" class="logo">
-        G-Flex
-      </RouterLink> 
+        <RouterLink :to="{ name: 'HomeView' }" class="logo">
+          G-Flex
+        </RouterLink> 
       <div class="right-links">
         <RouterLink :to="{ name: 'GenreView' }">
-        <button>
-          G플리
-        </button>
-      </RouterLink>
-        <RouterLink :to="{ name: 'LogInView' }">
-          <button>
-          로그인
-        </button>
-      </RouterLink> 
-      <RouterLink :to="{ name: 'HomeView' }">
-          <button @click="logOut">
-          로그아웃
-        </button>
-      </RouterLink> 
-        <RouterLink :to="{ name: 'SignUpView' }">
-          <button>
-            회원가입
-        </button>
-      </RouterLink> 
+          <button v-if="store.isLogin">
+            G플리
+          </button>
+        </RouterLink>
+        <template v-if="!store.isLogin">
+          <RouterLink :to="{ name: 'LogInView' }">
+            <button>
+              로그인
+            </button>
+          </RouterLink> 
+          <RouterLink :to="{ name: 'SignUpView' }">
+            <button>
+              회원가입
+            </button>
+          </RouterLink>
+        </template>
+        <template v-else>
+          <RouterLink :to="{ name: 'HomeView' }">
+            <button @click="logOut">
+              로그아웃
+            </button>
+          </RouterLink> 
+        </template>
         <RouterLink :to="{ name: 'ProfileView' }">
-          <button>
+          <button  v-if="store.isLogin">
             마이페이지
-        </button>
-      </RouterLink> 
+          </button>
+        </RouterLink> 
       </div>
     </div>
-
     <RouterView />
   </div>
 </template>
 
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-
-import { useMovieStore } from './stores/movie';
+import { useMovieStore } from './stores/movie'
 
 const store = useMovieStore()
 
@@ -51,7 +53,6 @@ const logOut = function() {
 
 <style>
 .navbar {
-  /* padding-left: 30%; */
   padding-top: 20px;
   display: flex;
   justify-content: space-between;
@@ -62,11 +63,10 @@ const logOut = function() {
   gap: 15px;
 }
 
-
 body {
   background-color: #000000;
-  color: #ffffff; /* 글자색을 흰색으로 설정 */
-  height: 100vh; /* 화면 전체 높이로 설정 */
+  color: #ffffff;
+  height: 100vh;
 }
 
 button {
@@ -79,10 +79,9 @@ button {
 }
 
 .logo {
-  font-size: 36px; 
+  font-size: 36px;
   color: white;
-  text-decoration: none; 
-  font-family: fantasy
+  text-decoration: none;
+  font-family: fantasy;
 }
-
 </style>
--- front/src/stores/movie.js
+++ front/src/stores/movie.js
@@ -3,19 +3,12 @@ import { defineStore } from 'pinia'
 import axios from 'axios'
 import { useRouter } from 'vue-router'
 
-
 export const useMovieStore = defineStore('Movie', () => {
   const API_URL = 'http://127.0.0.1:8000'
   const token = ref(null)
 
   const router = useRouter()
-  const isLogin = computed(() => {
-    if (token.value === null) {
-      return false
-    } else {
-      return true
-    }
-  })
+  const isLogin = computed(() => token.value !== null)
 
   const signUp = function (payload) {
     const { username, password1, password2 } = payload
@@ -24,20 +17,19 @@ export const useMovieStore = defineStore('Movie', () => {
       method: 'post',
       url: `${API_URL}/accounts/signup/`,
       data: {
-        username: username,
-        password1: password1,
-        password2: password2,
+        username,
+        password1,
+        password2,
       }
     })
     .then((response) => {
-      console.log('회원가입 성공 !')
-      router.push({ name:'LogInView' })
+      console.log('회원가입 성공!')
+      router.push({ name: 'LogInView' })
     })
     .catch((error) => {
       console.log('회원가입 실패:', error.response.data)
     })
   }
-
 
   const logIn = function (payload) {
     const { username, password } = payload
@@ -45,27 +37,25 @@ export const useMovieStore = defineStore('Movie', () => {
       method: 'post',
       url: `${API_URL}/accounts/login/`,
       data: {
-        username, password
+        username,
+        password,
       }
     })
-      .then((response) => {
-        console.log('로그인 성공')
-        console.log(response.data.key)
-        token.value = response.data.key
-        router.push({ name : 'HomeView' })
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    .then((response) => {
+      console.log('로그인 성공')
+      console.log(response.data)
+      token.value = response.data.key
+      router.push({ name: 'HomeView' })
+    })
+    .catch((error) => {
+      console.log('로그인 실패:', error)
+    })
   }
 
   const logOut = function() {
     token.value = null
-    router.push({ name : 'HomeView' })
+    router.push({ name: 'HomeView' })
   }
 
-  return { signUp, API_URL, logIn, token, isLogin, logOut }
-},
-{ persist: true }
-)
-
+  return { signUp, API_URL, logIn, logOut, token, isLogin }
+}, { persist: true })
